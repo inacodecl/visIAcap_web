e1979500908dc7083a313267f6a429c18d0b1af7
Jarod Pinto <jarodpintojarol2@gmail.com>
2025-12-02 13:06:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.inacap.visiacap',
+  appName: 'visiacap-app',
+  webDir: 'www'
+};
+
+export default config;
