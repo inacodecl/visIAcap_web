50bd89388c30cfdc9ca8ed1eed0d726e410edfd2
Benjinho-19 <benjamin.gonzalez119@inacapmail.cl>
2026-04-01 18:17:36 -0300

    Apartado de desarrolladores casi casi listo

--- src/app/features/public/home/components/geometric-overlay-top/geometric-overlay-top.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-geometric-overlay-top',
-    templateUrl: './geometric-overlay-top.component.html',
-    styleUrls: ['./geometric-overlay-top.component.scss'],
-    standalone: true
-})
-export class GeometricOverlayTopComponent { }
