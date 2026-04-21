e68a8dc03f0a44ae8858fcf285d69b4bad24f506
Jarod Pinto <jarodpintojarol2@gmail.com>
2026-04-21 16:41:49 -0400

    Carrusel horizontal en la seccion de noticias en /futuro

--- src/app/features/public/futuro/components-futuro/noticias-futuro/noticias-futuro.component.ts
+++ src/app/features/public/futuro/components-futuro/noticias-futuro/noticias-futuro.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { newspaperOutline, arrowForwardOutline } from 'ionicons/icons';
+import { newspaperOutline, arrowForwardOutline, arrowBackOutline } from 'ionicons/icons';
 import { Noticia } from '../../futuro.models';
 
 @Component({
@@ -11,12 +11,24 @@ import { Noticia } from '../../futuro.models';
   templateUrl: './noticias-futuro.component.html',
   styleUrls: ['./noticias-futuro.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslateModule]
+  imports: [CommonModule, TranslateModule, IonIcon]
 })
 export class NoticiasFuturoComponent {
   @Input() noticias: Noticia[] = [];
+  @ViewChild('carousel') carousel!: ElementRef<HTMLDivElement>;
 
   constructor() {
-    addIcons({ newspaperOutline, arrowForwardOutline });
+    addIcons({ newspaperOutline, arrowForwardOutline, arrowBackOutline });
+  }
+
+  scrollNews(direction: 'prev' | 'next') {
+    const element = this.carousel.nativeElement;
+    const scrollAmount = element.offsetWidth * 0.8;
+    
+    if (direction === 'prev') {
+      element.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    } else {
+      element.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
   }
 }
