05259d365a25d74008a4b1fd1481982a9feb02ed
nicooG0603 <nico.garcia22@gmail.com>
2026-04-27 13:48:36 -0400

    Creación responsividad futuro para celular

--- src/app/features/public/futuro/components-futuro/noticias-futuro/noticias-futuro.component.ts
+++ src/app/features/public/futuro/components-futuro/noticias-futuro/noticias-futuro.component.ts
@@ -1,34 +1,26 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { newspaperOutline, arrowForwardOutline, arrowBackOutline } from 'ionicons/icons';
 import { Noticia } from '../../futuro.models';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-noticias-futuro',
   templateUrl: './noticias-futuro.component.html',
   styleUrls: ['./noticias-futuro.component.scss'],
   standalone: true,
-  imports: [CommonModule, TranslateModule, IonIcon]
+  imports: [CommonModule, TranslateModule, IonIcon],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NoticiasFuturoComponent {
   @Input() noticias: Noticia[] = [];
-  @ViewChild('carousel') carousel!: ElementRef<HTMLDivElement>;
 
   constructor() {
     addIcons({ newspaperOutline, arrowForwardOutline, arrowBackOutline });
-  }
-
-  scrollNews(direction: 'prev' | 'next') {
-    const element = this.carousel.nativeElement;
-    const scrollAmount = element.offsetWidth * 0.8;
-    
-    if (direction === 'prev') {
-      element.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-    } else {
-      element.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-    }
   }
 }
--- src/app/features/public/futuro/components-futuro/proyectos-futuro/proyectos-futuro.component.ts
+++ src/app/features/public/futuro/components-futuro/proyectos-futuro/proyectos-futuro.component.ts
@@ -1,46 +1,26 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonIcon } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
 import { bulbOutline, arrowBackOutline, arrowForwardOutline } from 'ionicons/icons';
 import { ProyectoFuturo } from '../../futuro.models';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-proyectos-futuro',
   templateUrl: './proyectos-futuro.component.html',
   styleUrls: ['./proyectos-futuro.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonIcon, TranslateModule]
+  imports: [CommonModule, IonIcon, TranslateModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProyectosFuturoComponent {
   @Input() proyectos: ProyectoFuturo[] = [];
-  @ViewChild('proyectosCarousel') proyectosCarouselRef!: ElementRef<HTMLElement>;
-  activeSlideIndex = 0;
 
   constructor() {
     addIcons({ bulbOutline, arrowBackOutline, arrowForwardOutline });
-  }
-
-  onCarouselScroll() {
-    const el = this.proyectosCarouselRef?.nativeElement;
-    if (!el) return;
-    
-    // En Proyectos, las tarjetas tienen min-width: 450px + gap: 40px = 490px aprox.
-    const cardWidth = 450 + 40; 
-    const index = Math.round(el.scrollLeft / cardWidth);
-    if (this.activeSlideIndex !== index) {
-      this.activeSlideIndex = index;
-    }
-  }
-
-  scrollProyectos(direction: 'prev' | 'next') {
-    const el = this.proyectosCarouselRef?.nativeElement;
-    if (!el) return;
-    const scrollAmount = el.clientWidth * 0.85;
-    el.scrollBy({
-        left: direction === 'next' ? scrollAmount : -scrollAmount,
-        behavior: 'smooth'
-    });
   }
 }
